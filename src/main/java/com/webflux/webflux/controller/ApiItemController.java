d91aaa15ea9b9e22ab0a122ed8b57a4b24766dba
easywaldoji <easywaldoji@gmail.com>
2021-07-18 03:17:14 +0900

    신규 아이템 추가 REST API 추가

--- src/main/java/com/webflux/webflux/controller/ApiItemController.java
+++ src/main/java/com/webflux/webflux/controller/ApiItemController.java
@@ -3,11 +3,12 @@ package com.webflux.webflux.controller;
 import com.webflux.webflux.cart.Item;
 import com.webflux.webflux.cart.ItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.net.URI;
 
 @RestController
 public class ApiItemController {
@@ -26,5 +27,12 @@ public class ApiItemController {
     @GetMapping("/api/items/{id}")
     public Mono<Item> findOne(@PathVariable String id) {
         return this.repository.findById(id);
+    }
+
+    @PostMapping("/api/items")
+    Mono<ResponseEntity<?>> addNewItem(@RequestBody Mono<Item> item) {
+        return item.flatMap(s -> this.repository.save(s))
+            .map(savedItem -> ResponseEntity.created(URI.create(String.format("/api/items/%s", savedItem.getId())))
+                .body(savedItem));
     }
 }
