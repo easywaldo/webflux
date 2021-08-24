328f434024b06d8044d85f331977cec56cf8dc86
easywaldoji <easywaldoji@gmail.com>
2021-08-24 23:36:14 +0900

    Hooks on debug test

--- src/test/java/com/webflux/webflux/WebfluxApplicationTests.java
+++ src/test/java/com/webflux/webflux/WebfluxApplicationTests.java
@@ -3,6 +3,12 @@ package com.webflux.webflux;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Hooks;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Scheduler;
+import reactor.core.scheduler.Schedulers;
+
+import java.util.Random;
 
 @SpringBootTest
 class WebfluxApplicationTests {
@@ -21,6 +27,22 @@ class WebfluxApplicationTests {
             .map(dish -> Dish.deliver(dish));
 
         dishFlux.subscribe();
+    }
+
+    @Test
+    public void hooks_on_debug_test() {
+        Hooks.onOperatorDebug();
+
+        Mono<Integer> source;
+        if (new Random().nextBoolean()) {
+            source = Flux.range(1, 10).elementAt(5);
+        }
+        else {
+            source = Flux.just(1,2,3,4,5).elementAt(10);
+        }
+
+        source.subscribeOn(Schedulers.parallel())
+            .block();
     }
 
 }
