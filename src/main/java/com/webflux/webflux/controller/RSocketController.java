d26413a0635297ebfc90effe9a493ba7ea0f8f74
easywaldoji <easywaldoji@gmail.com>
2021-10-22 23:53:21 +0900

    Async Http Url 추가

--- src/main/java/com/webflux/webflux/controller/RSocketController.java
+++ src/main/java/com/webflux/webflux/controller/RSocketController.java
@@ -1,18 +1,31 @@
 package com.webflux.webflux.controller;
 
 import com.webflux.webflux.cart.Item;
+import io.netty.channel.ChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.handler.timeout.WriteTimeoutHandler;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.messaging.rsocket.RSocketRequester;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+import reactor.netty.http.client.HttpClient;
 
 import java.net.URI;
 import java.time.Duration;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 import static io.rsocket.metadata.WellKnownMimeType.MESSAGE_RSOCKET_ROUTING;
 import static org.springframework.http.MediaType.*;
@@ -68,5 +81,35 @@ public class RSocketController {
             .flatMapMany(rSocketRequester -> rSocketRequester
                 .route("newItems.monitor")
                 .retrieveFlux(Item.class));
+    }
+
+    @Async
+    @GetMapping(value = "/asyncHttp")
+    public String asyncHttp() throws ExecutionException, InterruptedException, TimeoutException {
+        HttpClient httpClient = HttpClient.create()
+            .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
+            .responseTimeout(Duration.ofMillis(5000))
+            .doOnConnected(conn ->
+                conn.addHandlerLast(new ReadTimeoutHandler(5000, TimeUnit.MILLISECONDS))
+                    .addHandlerLast(new WriteTimeoutHandler(5000, TimeUnit.MILLISECONDS)));
+        WebClient client = WebClient.builder()
+            .clientConnector(new ReactorClientHttpConnector(httpClient))
+            .build();
+        WebClient.UriSpec<WebClient.RequestBodySpec> uriSpec = client.method(HttpMethod.POST);
+        WebClient.RequestBodySpec bodySpec = uriSpec.uri("https://m.naver.com");
+        WebClient.RequestHeadersSpec headersSpec = bodySpec.body(
+            BodyInserters.fromPublisher(Mono.just("data"), String.class)
+        );
+
+        Mono<String> response = headersSpec.retrieve()
+            .bodyToMono(String.class)
+            .doOnSuccess(System.out::print)
+            .doOnNext(System.out::print)
+            .subscribeOn(Schedulers.immediate());
+
+        String futureHtml = "";
+        futureHtml = response.toFuture().get(1000L, TimeUnit.MILLISECONDS);
+
+        return futureHtml;
     }
 }
