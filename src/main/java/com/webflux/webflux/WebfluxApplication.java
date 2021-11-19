263ea1f7717b0c35436c69dcc684140365f8f6aa
easywaldoji <easywaldoji@gmail.com>
2021-11-19 23:12:34 +0900

    초기데이터 설정

--- src/main/java/com/webflux/webflux/WebfluxApplication.java
+++ src/main/java/com/webflux/webflux/WebfluxApplication.java
@@ -1,7 +1,11 @@
 package com.webflux.webflux;
 
+import com.webflux.webflux.cart.Item;
+import com.webflux.webflux.cart.ItemRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
@@ -12,6 +16,20 @@ public class WebfluxApplication {
             TemplateEngine.class.getCanonicalName(), "process")
             .install();*/
         SpringApplication.run(WebfluxApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner demo(
+        ItemRepository itemRepository) {
+
+        return (args) -> {
+            itemRepository.deleteAll();
+            itemRepository.save(Item.builder()
+                .itemName("iphone")
+                .id("ipsx-13")
+                .itemPrice(130000L)
+                .build());
+        };
     }
 
 }
